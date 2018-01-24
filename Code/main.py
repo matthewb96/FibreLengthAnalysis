ae1a8c2da36a27254222399fc6cf88e230aabd46
Matthew <32818215+matthewb96@users.noreply.github.com>
2018-01-24 17:50:05 +0000

    Tested with 4 fibres and 16 fibres
    Tested with 4 and 16 fibres and works correctly for both finding 4 and 16 lengths respectively. All the lengths found were correct,. However it is very inefficient with 4 fibres taking ~15s and 16 taking ~360s.

--- Code/main.py
+++ Code/main.py
@@ -95,7 +95,7 @@ def checkLine(pos1, pos2):
     else: #The line has a gradient
         gradient = (pos1[1]-pos2[1])/(pos1[0]-pos2[0])
         yIntercept = pos1[1] - (gradient*pos1[0])
-        if np.rint(yIntercept) != np.rint(pos2[1] - (gradient*pos2[0])):
+        if np.rint(yIntercept) != np.rint(pos2[1] - (gradient*pos2[0])): #Rounding errors can cause these to not be equal so rounded to int for checking
             print("Cannot find the equation for the line between " + str(pos1) + " and " + str(pos2))
             return False
     
